e387237aa3f2ef4d6b6df0ec5a1df19ccb48fa1c
Juan <juan.nascimento.999@gmail.com>
2022-11-18 14:52:06 -0300

    Update

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -28,10 +28,10 @@ public class SellerDaoJDBC implements SellerDao {
 	public void insert(Seller obj) {
 		PreparedStatement st = null;
 		try {
-			st = conn.prepareStatement("INSERT INTO seller"
-					+ "(Name,Email,BirthDate,BaseSalary,DepartmentId)"
-					+ "VALUES"
-					+ "(?,?,?,?,?)",
+			st = conn.prepareStatement("INSERT INTO seller "
+					+ "(Name,Email,BirthDate,BaseSalary,DepartmentId) "
+					+ "VALUES "
+					+ "(?,?,?,?,?) ",
 					Statement.RETURN_GENERATED_KEYS);
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
@@ -64,8 +64,28 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
-
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id = ? ");
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
@@ -126,7 +146,7 @@ public class SellerDaoJDBC implements SellerDao {
 					"SELECT seller.*,department.Name as DepName " 
 						    + "FROM seller INNER JOIN department "
 						    + "ON seller.DepartmentId = department.Id " 
-						    + "ORDER BY Name");
+						    + "ORDER BY Name ");
 
 
 			rs = st.executeQuery();
